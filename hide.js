16ff23fa3a869bd7273e5b6779ab95d05916f245
mex <65974823+BasicllyMaximqs@users.noreply.github.com>
2021-12-11 17:51:43 +0100

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,7 +1,3 @@
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
-
 while(document.getElementById("hide-obj")){
         var elem = document.getElementById("hide-obj");
         elem.parentNode.removeChild(elem);
